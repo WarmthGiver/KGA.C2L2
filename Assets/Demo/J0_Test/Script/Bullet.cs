eb12c6d920c09d7fdd61b547f7ef2b743b63438f
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-01-16 15:53:12 +0900

    [Refactor]총알 조건부 Destroy를 따로 함수로 뺐음

--- Assets/Demo/J0_Test/Script/Bullet.cs
+++ Assets/Demo/J0_Test/Script/Bullet.cs
@@ -20,15 +20,19 @@ public class Bullet : MonoBehaviour
         bulletRigid.AddForce(transform.up.normalized * speed, ForceMode2D.Impulse);
 
         // 범위 밖 나가면 지우기
-        if (transform.position.x < -20f || transform.position.x > 20f || transform.position.y < -20f || transform.position.y > 20f)
-        {
-            Destroy(gameObject);
-        }
+        DestroyBullet();
     }
 
     public void Initialize(int damage, float speed)
     {
         this.damage = damage;
         this.speed = speed;
+    }
+    public void DestroyBullet()
+    {
+        if (transform.position.x < -10f || transform.position.x > 10f || transform.position.y < -10f || transform.position.y > 10f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
