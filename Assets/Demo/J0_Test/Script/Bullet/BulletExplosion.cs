41443f03b982afeac130ece2a6b74769b1a0ba41
2jaeyoung2 <80027417+2jaeyoung2@users.noreply.github.com>
2025-01-22 16:44:27 +0900

    [Fix]폭발 데미지 들어가게 수정

--- Assets/Demo/J0_Test/Script/Bullet/BulletExplosion.cs
+++ Assets/Demo/J0_Test/Script/Bullet/BulletExplosion.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class BulletExplosion : Bullet
 {
     [SerializeField]
-    protected GameObject explosion;
+    protected Explosion explosionPrefab;
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         // 적 닿으면 폭발이 일어나고 사라짐
         if (collision.CompareTag("Enemy"))
         {
-            Instantiate(explosion, transform.position, transform.rotation);
-            collision.GetComponent<IDamageable>()?.GetDamage(damage);
+            var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+            explosion.Initialize(damage);
             gameObject.SetActive(false);
         }
     }
